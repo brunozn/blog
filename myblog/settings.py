5e962a6a29b69c8a188a54ed56ea556e4d256d74
Bruno José <46513538+brunozn@users.noreply.github.com>
2020-04-12 21:45:08 -0300

    adicionando o localhost a ALLOWED_HOST

--- myblog/settings.py
+++ myblog/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -25,9 +24,8 @@ SECRET_KEY = '2+y*058#e7q(qqb(6x&2*$#9b)w58%&j_44i^8yjw1*$a1c^8t'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['brunozn.pythonanywhere.com']
-#, '127.0.0.1'
-
+ALLOWED_HOSTS = ['brunozn.pythonanywhere.com', '127.0.0.1']
+# , '127.0.0.1'
 # Application definition
 
 INSTALLED_APPS = [
@@ -70,7 +68,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'myblog.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
@@ -80,7 +77,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -100,7 +96,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
@@ -113,7 +108,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
